f6ccbe215bd4a79f2186fbbe157e99c51fef4be0
Taga Yasunori <yasukun.taga@gmail.com>
2019-09-01 18:12:59 +0900

    add sample btc address

--- web/src/components/Footer.vue
+++ web/src/components/Footer.vue
@@ -3,8 +3,12 @@
   <v-layout justify-center>
     <v-flex xs12 md6>
       <v-row align="center" justify="center" >
+        <span class="caption">Sample Address : 3HXM3iSGWzuXwHYzyquhmtMn2A5uXdLgQh</span>
+      </v-row>
+      <v-row align="center" justify="center" >
         <span class="caption"><v-icon class="pr-1">mdi-github-face</v-icon><a href="https://github.com/yassun/crypto-qrcode-generator" target="_blank">by yasuun</a><v-icon class="pl-1">mdi-github-face</v-icon></span>
       </v-row>
+
     </v-flex>
   </v-layout>
 </v-container>
