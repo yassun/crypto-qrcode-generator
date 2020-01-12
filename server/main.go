59d7d0b99af47dda0ad1cd86ce8663f6602ab5a9
Taga Yasunori <yasukun.taga@gmail.com>
2020-01-12 12:59:17 +0900

    deploy heroku

--- server/main.go
+++ server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"server/handler"
 	"server/validator"
 
@@ -9,10 +10,11 @@ import (
 )
 
 func main() {
+	port := os.Args[1]
 	e := echo.New()
 	e.Use(middleware.CORS())
 	e.Validator = validator.NewCustomValidator()
 	e.GET("/heartbeat", handler.HandleHeartbeat)
 	e.POST("/api/v1/generate-qr/btc", handler.GenerateBtcQR)
-	e.Logger.Fatal(e.Start(":8000"))
+	e.Logger.Fatal(e.Start(":" + port))
 }
